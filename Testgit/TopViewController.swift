cd64114f4304e18436e7a8aeb535d500c39932c2
生沼佑樹 <oinumayu-ki@namanumayuukinoMacBook-Air.local>
2019-08-01 15:12:20 +0900

    add topVC , modify main.storyboard

---
+++ Testgit/TopViewController.swift
@@ -0,0 +1,31 @@
+//
+//  TopViewController.swift
+//  Testgit
+//
+//  Created by 生沼佑樹 on 2019/08/01.
+//  Copyright © 2019 生沼佑樹. All rights reserved.
+//
+
+import UIKit
+import PGFramework
+
+// MARK: - Property
+class TopViewController: BaseViewController {
+    
+}
+
+// MARK: - Life cycle
+extension TopViewController {
+    
+}
+
+// MARK: - Protocol
+extension TopViewController {
+    
+}
+
+// MARK: - method
+extension TopViewController {
+    
+}
+
